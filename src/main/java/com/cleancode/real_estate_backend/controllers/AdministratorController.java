6f8a53b653f8ea29371ccee077a233bd456c8988
R0bert196 <robertchiriac196@yahoo.com>
2024-06-25 22:14:22 +0300

    capture create building request payload

---
+++ src/main/java/com/cleancode/real_estate_backend/controllers/AdministratorController.java
@@ -0,0 +1,23 @@
+package com.cleancode.real_estate_backend.controllers;
+
+import com.cleancode.real_estate_backend.dtos.administrator.building.BuildingRequestDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/administrator")
+@RequiredArgsConstructor
+public class AdministratorController {
+
+    @PostMapping("/building")
+    public void addBuilding(@RequestBody BuildingRequestDTO buildingRequestDTO) {
+
+        System.out.println(buildingRequestDTO);
+
+    }
+}
---
+++ src/main/java/com/cleancode/real_estate_backend/dtos/administrator/building/FloorRequestDTO.java
@@ -0,0 +1,7 @@
+package com.cleancode.real_estate_backend.dtos.administrator.building;
+
+public record FloorRequestDTO(
+        String floorSize,
+        String floorNumber
+) {
+}
