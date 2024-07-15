df3507fc66ca01d9fc6fc3bd2191e14ef7bbcc41
R0bert196 <robertchiriac196@yahoo.com>
2024-07-15 10:17:32 +0300

    create ticket response dto view

---
+++ src/main/java/com/cleancode/real_estate_backend/dtos/administrator/ticket/response/TicketResponseDTOView.java
@@ -0,0 +1,11 @@
+package com.cleancode.real_estate_backend.dtos.administrator.ticket.response;
+
+public record TicketResponseDTOView(
+        Long id,
+        String severity,
+        String status,
+        Long creatorId,
+        String creatorName,
+        String department
+) {
+}
