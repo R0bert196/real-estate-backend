5df93083eeecbede52ccf6973d64986084c3dcc6
R0bert196 <robertchiriac196@yahoo.com>
2024-08-07 11:24:03 +0300

    add logging to FloorService

--- src/main/java/com/cleancode/real_estate_backend/services/FloorService.java
+++ src/main/java/com/cleancode/real_estate_backend/services/FloorService.java
@@ -7,22 +7,33 @@ import com.cleancode.real_estate_backend.entities.Building;
 import com.cleancode.real_estate_backend.entities.Floor;
 import com.cleancode.real_estate_backend.repositories.FloorRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
+@Log4j2
 @RequiredArgsConstructor
 public class FloorService {
 
     private final FloorRepository floorRepository;
 
-
     public FloorResponseDTOLite convertToDTOLite(Floor entity) {
-        return new FloorResponseDTOLite(entity.getSize(), entity.getAvailableSize(), entity.getFloorNumber(), entity.getId());
+        log.info("Converting Floor entity to FloorResponseDTOLite: Floor ID = {}", entity.getId());
+
+        FloorResponseDTOLite dtoLite = new FloorResponseDTOLite(
+                entity.getSize(),
+                entity.getAvailableSize(),
+                entity.getFloorNumber(),
+                entity.getId()
+        );
+
+        log.info("Converted FloorResponseDTOLite: {}", dtoLite);
+        return dtoLite;
     }
 
     public FloorResponseDTO convertToDTO(Floor entity) {
+        log.info("Converting Floor entity to FloorResponseDTO: Floor ID = {}", entity.getId());
 
         Building building = entity.getBuilding();
 
@@ -30,17 +41,18 @@ public class FloorService {
                 building.getName(),
                 null,
                 null,
-                building.getId());
+                building.getId()
+        );
 
-        return new FloorResponseDTO(
+        FloorResponseDTO dto = new FloorResponseDTO(
                 entity.getSize(),
                 entity.getFloorNumber(),
                 entity.getId(),
                 entity.getAvailableSize(),
                 buildingResponseDTOLite
-
         );
 
-
+        log.info("Converted FloorResponseDTO: {}", dto);
+        return dto;
     }
 }
