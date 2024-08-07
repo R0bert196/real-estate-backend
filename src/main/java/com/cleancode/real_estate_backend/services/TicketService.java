7d1e409ab69a283c99b94a0ba7f3c9c8db0e87ca
R0bert196 <robertchiriac196@yahoo.com>
2024-08-07 11:31:47 +0300

    add logging to TicketService

--- src/main/java/com/cleancode/real_estate_backend/services/TicketService.java
+++ src/main/java/com/cleancode/real_estate_backend/services/TicketService.java
@@ -24,6 +24,8 @@ import com.cleancode.real_estate_backend.utils.IAuthenticationFacade;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +36,7 @@ import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class TicketService {
     private final TicketRepository ticketRepository;
     private final AppUserRepository appUserRepository;
@@ -43,23 +46,41 @@ public class TicketService {
     private final IAuthenticationFacade authenticationFacade;
 
     public List<TicketResponseDTOView> getTicketsViewManager(Pageable pageable) {
+        String userEmail = authenticationFacade.getAuthentication().getName();
+        log.info("Fetching tickets for manager: {}", userEmail);
 
-        AppUser manager = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName()).orElseThrow(EntityNotFoundException::new);
+        AppUser manager = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> {
+                    log.error("Manager not found: {}", userEmail);
+                    return new EntityNotFoundException("Manager not found: " + userEmail);
+                });
 
-
-        return ticketRepository.findAllWithCreatorByManagerId(pageable, manager.getId()).stream().map(this::convertToDTOView).toList();
+        return ticketRepository.findAllWithCreatorByManagerId(pageable, manager.getId())
+                .stream()
+                .map(this::convertToDTOView)
+                .toList();
     }
 
     public List<TicketResponseDTOView> getTicketsViewTenant(Pageable pageable) {
+        String userEmail = authenticationFacade.getAuthentication().getName();
+        log.info("Fetching tickets for tenant: {}", userEmail);
 
-        AppUser representant = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName()).orElseThrow(EntityNotFoundException::new);
+        AppUser representant = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> {
+                    log.error("Representant not found: {}", userEmail);
+                    return new EntityNotFoundException("Representant not found: " + userEmail);
+                });
 
-        return ticketRepository.findAllWithCreatorByRepresentantId(pageable, representant.getId()).stream().map(this::convertToDTOView).toList();
+        return ticketRepository.findAllWithCreatorByRepresentantId(pageable, representant.getId())
+                .stream()
+                .map(this::convertToDTOView)
+                .toList();
     }
 
     private TicketResponseDTOView convertToDTOView(Ticket ticket) {
+        log.info("Converting Ticket entity to TicketResponseDTOView: Ticket ID = {}", ticket.getId());
 
-        return new TicketResponseDTOView(
+        TicketResponseDTOView dto = new TicketResponseDTOView(
                 ticket.getId(),
                 ticket.getSubject(),
                 String.valueOf(ticket.getSeverity()),
@@ -68,36 +89,32 @@ public class TicketService {
                 ticket.getCreator().getUsername(),
                 String.valueOf(ticket.getDepartment())
         );
-    }
 
-//    private Ticket convertToEntity(TicketRequestDTO dto) {
-//
-//        // Assume current user is the creator
-////        AppUser creator = appUserRepository.findCurrentLoggedInUser();
-//
-//        Ticket entity = Ticket.builder()
-//                .subject(dto.subject())
-//                .severity(TicketSeverity.valueOf(dto.severity()))
-//                .status(TicketStatus.PENDING) // Default status
-////                .creator(creator)
-//                .imageUrls(dto.imageUrls())
-//                .build();
-//
-//        return entity;
-//    }
+        log.info("Converted TicketResponseDTOView: {}", dto);
+        return dto;
+    }
 
     @Transactional
     public TicketResponseDTOLite addTicket(TicketRequestDTO ticketRequestDTO) {
+        String userEmail = authenticationFacade.getAuthentication().getName();
+        log.info("Adding ticket for user: {}", userEmail);
 
-        AppUser creator = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName()).orElseThrow(EntityNotFoundException::new);
+        AppUser creator = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> {
+                    log.error("Creator not found: {}", userEmail);
+                    return new EntityNotFoundException("Creator not found: " + userEmail);
+                });
 
         TicketMessage ticketMessage = TicketMessage.builder()
                 .message(ticketRequestDTO.message())
                 .creator(creator)
                 .build();
 
-        RentedFloor rentedFloor = rentedFloorRepository.findById(ticketRequestDTO.rentedFloorId()).orElseThrow(EntityNotFoundException::new);
-
+        RentedFloor rentedFloor = rentedFloorRepository.findById(ticketRequestDTO.rentedFloorId())
+                .orElseThrow(() -> {
+                    log.error("Rented floor not found: {}", ticketRequestDTO.rentedFloorId());
+                    return new EntityNotFoundException("Rented floor not found: " + ticketRequestDTO.rentedFloorId());
+                });
 
         Ticket ticket = new Ticket();
         ticket.setSubject(ticketRequestDTO.subject());
@@ -105,27 +122,33 @@ public class TicketService {
         ticket.setRentedFloor(rentedFloor);
         ticket.setStatus(TicketStatus.PENDING);
         ticket.setDepartment(TicketDepartment.valueOf(ticketRequestDTO.department()));
-
         ticket.setCreator(creator);
-
-        AppUser manager = rentedFloor.getFloor().getBuilding().getManager();
-        ticket.setResponsibleManager(manager);
+        ticket.setResponsibleManager(rentedFloor.getFloor().getBuilding().getManager());
 
         Ticket savedTicket = ticketRepository.save(ticket);
+        log.info("Saved new ticket: {}", savedTicket);
 
         ticketMessage.setTicket(savedTicket);
-
         TicketMessage savedTicketMessage = ticketMessageRepository.save(ticketMessage);
+        log.info("Saved new ticket message: {}", savedTicketMessage);
 
         return new TicketResponseDTOLite(savedTicket.getId(), savedTicketMessage.getId(), ticket.getSubject());
     }
 
     public TicketResponseDTO getTicket(Long ticketId) {
+        log.info("Fetching ticket details for ticket ID: {}", ticketId);
 
-        Ticket ticket = ticketRepository.findWithCreatorById(ticketId).orElseThrow(EntityNotFoundException::new);
+        Ticket ticket = ticketRepository.findWithCreatorById(ticketId)
+                .orElseThrow(() -> {
+                    log.error("Ticket not found: {}", ticketId);
+                    return new EntityNotFoundException("Ticket not found: " + ticketId);
+                });
 
         AppUser loggedUser = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName())
-                .orElseThrow(EntityNotFoundException::new);
+                .orElseThrow(() -> {
+                    log.error("Logged user not found: {}", authenticationFacade.getAuthentication().getName());
+                    return new EntityNotFoundException("Logged user not found: " + authenticationFacade.getAuthentication().getName());
+                });
 
         Role userRole = loggedUser.getRole().stream().findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("User has no assigned roles"));
@@ -133,6 +156,7 @@ public class TicketService {
         switch (userRole) {
             case ROLE_MANAGER:
                 if (!ticket.getResponsibleManager().equals(loggedUser)) {
+                    log.error("User {} is not the manager responsible for ticket ID: {}", loggedUser.getEmail(), ticketId);
                     throw new IllegalArgumentException("Only the manager of this ticket can view the details");
                 }
                 break;
@@ -140,32 +164,33 @@ public class TicketService {
                 boolean isRepresentant = appUserRepository.findTicketTenantRepresentatns().stream()
                         .anyMatch(appUser -> appUser.equals(loggedUser));
                 if (!isRepresentant) {
+                    log.error("User {} is not the representant responsible for ticket ID: {}", loggedUser.getEmail(), ticketId);
                     throw new IllegalArgumentException("Only the responsible person of this ticket can view the details");
                 }
                 break;
             default:
+                log.error("User {} doesn't have sufficient permission", loggedUser.getEmail());
                 throw new IllegalArgumentException("User doesn't have sufficient permission");
         }
 
-
         List<TicketMessage> ticketMessages = ticketMessageRepository.findAllWithImageUrlsByTicket_Id(ticket.getId());
-
         List<TicketMessageResponseDTO> ticketMessageResponseDTOS = new ArrayList<>();
 
         ticketMessages.forEach(ticketMessage -> {
-
             List<byte[]> photos;
 
             try {
                 photos = photoService.getPhotos(ticketMessage.getImageUrls());
             } catch (IOException e) {
+                log.error("Error reading photos for ticket message ID: {}", ticketMessage.getId(), e);
                 throw new RuntimeException(e);
             }
 
             AppUserResponseDTOLite messageCreator = new AppUserResponseDTOLite(
                     ticketMessage.getCreator().getId(),
                     ticketMessage.getCreator().getEmail(),
-                    ticketMessage.getCreator().getName());
+                    ticketMessage.getCreator().getName()
+            );
 
             TicketMessageResponseDTO ticketMessageResponseDTO = new TicketMessageResponseDTO(
                     ticketMessage.getId(),
@@ -176,16 +201,15 @@ public class TicketService {
             );
 
             ticketMessageResponseDTOS.add(ticketMessageResponseDTO);
-
         });
 
         AppUserResponseDTOLite ticketCreator = new AppUserResponseDTOLite(
                 ticket.getCreator().getId(),
                 ticket.getCreator().getEmail(),
-                ticket.getCreator().getName());
+                ticket.getCreator().getName()
+        );
 
-
-        return new TicketResponseDTO(
+        TicketResponseDTO ticketResponseDTO = new TicketResponseDTO(
                 ticket.getId(),
                 String.valueOf(ticket.getSeverity()),
                 String.valueOf(ticket.getStatus()),
@@ -194,38 +218,64 @@ public class TicketService {
                 ticketCreator,
                 ticketMessageResponseDTOS
         );
+
+        log.info("Fetched ticket details: {}", ticketResponseDTO);
+        return ticketResponseDTO;
     }
 
     public void addPhotosUrlsToMessage(Long ticketMessageId, Set<String> imageUrls) {
-        TicketMessage ticketMessage = ticketMessageRepository.findById(ticketMessageId).orElseThrow(EntityNotFoundException::new);
+        log.info("Adding photo URLs to ticket message ID: {}", ticketMessageId);
+
+        TicketMessage ticketMessage = ticketMessageRepository.findById(ticketMessageId)
+                .orElseThrow(() -> {
+                    log.error("Ticket message not found: {}", ticketMessageId);
+                    return new EntityNotFoundException("Ticket message not found: " + ticketMessageId);
+                });
+
         ticketMessage.setImageUrls(imageUrls);
         ticketMessageRepository.save(ticketMessage);
+        log.info("Added photo URLs to ticket message ID: {}", ticketMessageId);
     }
 
     public Long countTickets() {
+        String userEmail = authenticationFacade.getAuthentication().getName();
+        log.info("Counting tickets for user: {}", userEmail);
 
-        AppUser loggedUser = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName()).orElseThrow(EntityNotFoundException::new);
+        AppUser loggedUser = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> {
+                    log.error("User not found: {}", userEmail);
+                    return new EntityNotFoundException("User not found: " + userEmail);
+                });
 
-        if (loggedUser.getRole().stream()
-                .anyMatch(role -> role == Role.ROLE_MANAGER)) {
-
-            return ticketRepository.countTicketByResponsibleManagerId(loggedUser.getId());
-        } else if (loggedUser.getRole().stream()
-                .anyMatch(role -> role == Role.ROLE_REPRESENTANT)) {
-
-            return ticketRepository.countTicketsByRepresentantId(loggedUser.getId());
+        Long count;
+        if (loggedUser.getRole().stream().anyMatch(role -> role == Role.ROLE_MANAGER)) {
+            count = ticketRepository.countTicketByResponsibleManagerId(loggedUser.getId());
+        } else if (loggedUser.getRole().stream().anyMatch(role -> role == Role.ROLE_REPRESENTANT)) {
+            count = ticketRepository.countTicketsByRepresentantId(loggedUser.getId());
+        } else {
+            log.error("User role not found for user: {}", userEmail);
+            throw new IllegalArgumentException("User role not found");
         }
 
-        throw new IllegalArgumentException("User not found");
-
-
+        log.info("Counted tickets for user {}: {}", userEmail, count);
+        return count;
     }
 
     public TicketResponseDTOLite addMessageToTicket(Long ticketId, TicketMessageRequestDTO requestDTO) {
+        String userEmail = authenticationFacade.getAuthentication().getName();
+        log.info("Adding message to ticket ID: {} by user: {}", ticketId, userEmail);
 
-        AppUser creator = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName()).orElseThrow(EntityNotFoundException::new);
+        AppUser creator = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> {
+                    log.error("Creator not found: {}", userEmail);
+                    return new EntityNotFoundException("Creator not found: " + userEmail);
+                });
 
-        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(EntityNotFoundException::new);
+        Ticket ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> {
+                    log.error("Ticket not found: {}", ticketId);
+                    return new EntityNotFoundException("Ticket not found: " + ticketId);
+                });
 
         TicketMessage ticketMessage = TicketMessage.builder()
                 .creator(creator)
@@ -234,33 +284,43 @@ public class TicketService {
                 .build();
 
         TicketMessage savedMessage = ticketMessageRepository.save(ticketMessage);
+        log.info("Added message to ticket: {}", savedMessage);
 
         return new TicketResponseDTOLite(ticket.getId(), savedMessage.getId(), ticket.getSubject());
     }
 
     public void updateTicket(Long ticketId, TicketUpdateRequestDTO ticketRequestDTO) {
+        String userEmail = authenticationFacade.getAuthentication().getName();
+        log.info("Updating ticket ID: {} by user: {}", ticketId, userEmail);
 
-        AppUser loggedUser = appUserRepository.findByEmail(authenticationFacade.getAuthentication().getName()).orElseThrow(EntityNotFoundException::new);
+        AppUser loggedUser = appUserRepository.findByEmail(userEmail)
+                .orElseThrow(() -> {
+                    log.error("User not found: {}", userEmail);
+                    return new EntityNotFoundException("User not found: " + userEmail);
+                });
 
-        Ticket foundTicket = ticketRepository.findById(ticketId).orElseThrow(EntityNotFoundException::new);
+        Ticket foundTicket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> {
+                    log.error("Ticket not found: {}", ticketId);
+                    return new EntityNotFoundException("Ticket not found: " + ticketId);
+                });
 
         if (!foundTicket.getResponsibleManager().equals(loggedUser)) {
+            log.error("User {} is not the manager responsible for ticket ID: {}", userEmail, ticketId);
             throw new IllegalArgumentException("Only the responsible manager of the ticket can update the ticket");
         }
 
         if (ticketRequestDTO.department() != null) {
-
             foundTicket.setDepartment(TicketDepartment.valueOf(ticketRequestDTO.department()));
         }
         if (ticketRequestDTO.severity() != null) {
-
             foundTicket.setSeverity(TicketSeverity.valueOf(ticketRequestDTO.severity()));
         }
         if (ticketRequestDTO.status() != null) {
-
             foundTicket.setStatus(TicketStatus.valueOf(ticketRequestDTO.status()));
         }
 
         ticketRepository.save(foundTicket);
+        log.info("Updated ticket ID: {}", ticketId);
     }
 }
